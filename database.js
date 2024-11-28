0e446a431b61cc30e210962a266d1fa80cb81b75
yurisdevops <yurimachado.dev@gmail.com>
2024-11-28 07:14:11 -0300

    Alteração postgre.

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 const { Pool } = require("pg");
 
-// Configurações da conexão com o banco de dados PostgreSQL
+// Configuração do pool usando a URL do banco de dados
 const pool = new Pool({
-  user: "yuridevops", // Substitua pelo seu usuário do PostgreSQL
-  host: "localhost", // Ou o host do seu servidor PostgreSQL
-  database: "db-devnotas", // Nome do banco de dados
-  password: "90352415", // Substitua pela sua senha
-  port: 5432, // Porta padrão do PostgreSQL
+  connectionString: process.env.DATABASE_URL, // Render define esta variável automaticamente
+  ssl: {
+    rejectUnauthorized: false, // Necessário para conexões SSL no Render
+  },
 });
 
 // Verificação e criação da tabela se não existir
